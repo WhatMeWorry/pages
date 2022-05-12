db850b237aa24bbd36887910c5343d40ef37f698
WhatMeWorry <WhatMeWorry@users.noreply.github.com>
2022-05-12 18:20:02 -0500

    Update TriangleCollision.js

--- Miscellaneous/TriangleCollision.js
+++ Miscellaneous/TriangleCollision.js
@@ -83,3 +83,78 @@ function triPoint(x1, y1, x2, y2, x3, y3, px, py)
   
     return false;
 }
+
+
+
+
+
+
+
+
+
+Or another way:
+
+p = {x: 0, y:0}
+
+      // three points of the triangle
+
+one   = {x: 300, y:100}
+two   = {x: 450, y:300}
+three = {x: 150, y:300}
+
+function setup() 
+{
+    createCanvas(600,400);
+    noCursor();
+    strokeWeight(5);   // make point easier to see
+}
+
+
+function draw() 
+{
+    background(255);
+
+    // mouse point to mouse coordinates
+    p.x = mouseX;
+    p.y = mouseY;
+
+    // check for collision
+    // if hit, change fill color
+
+    hit = triPoint(one, two, three, p);
+    if (hit) 
+        fill(255,150,0);
+    else
+        fill(0,150,255);
+
+    noStroke();
+    triangle(one.x, one.y, two.x,two.y, three.x, three.y);
+
+    // draw the point
+    stroke(0, 150);
+    point(p.x,p.y);  
+}
+
+
+// TRIANGLE/POINT
+function triPoint(one, two, three, p) 
+{
+    // get the area of the triangle
+    areaOrig = abs( (two.x-one.x)*(three.y-one.y) - (three.x-one.x)*(two.y-one.y) );
+
+    // get the area of 3 triangles made between the point
+    // and the corners of the triangle
+    area1 = abs( (one.x  -p.x)*(  two.y-p.y) - (two.x  -p.x)*(  one.y-p.y) );
+    area2 = abs( (two.x  -p.x)*(three.y-p.y) - (three.x-p.x)*(  two.y-p.y) );
+    area3 = abs( (three.x-p.x)*(  one.y-p.y) -  (one.x -p.x)*(three.y-p.y) );
+
+    // if the sum of the three areas equals the original,
+    // we're inside the triangle!
+  
+    if ( (area1 + area2 + area3) == areaOrig) 
+    {
+        return true;
+    }
+  
+    return false;
+}
